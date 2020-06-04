090a03cb77c00798faf22ed87d3a468973e58b3c
Michael Ritchie <mister@thanksmister.com>
2020-06-04 10:43:00 -0300

    updated copyright

--- app/src/main/java/com/thanksmister/bitcoin/localtrader/architecture/ProgressBarMessage.java
+++ app/src/main/java/com/thanksmister/bitcoin/localtrader/architecture/ProgressBarMessage.java
@@ -1,8 +1,16 @@
 /*
  * Copyright (c) 2020 ThanksMister LLC
- * www.ThanksMister.com
- * mister@thanksmister.com
- * Mozilla Public License Version 2.0
+ *  http://www.thanksmister.com
+ *
+ *  Mozilla Public License 2.0
+ *
+ *  Permissions of this weak copyleft license are conditioned on making
+ *  available source code of licensed files and modifications of those files
+ *  under the same license (or in certain cases, one of the GNU licenses).
+ *  Copyright and license notices must be preserved. Contributors provide
+ *  an express grant of patent rights. However, a larger work using the
+ *  licensed work may be distributed under different terms and without source
+ *  code for files added in the larger work.
  */
 
 package com.thanksmister.bitcoin.localtrader.architecture;
